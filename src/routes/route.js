977c2dc6b3999dc4c098096a068b7e34b1d567e5
Rushikesh Venkatrao Galchelwar <98635628+Sonu0911@users.noreply.github.com>
2022-09-16 13:53:39 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,7 @@ const userController = require('../controller/userController')
 
 
 router.post("/register", userController.createUser)
+router.post("/login", userController.loginUser)
 router.get("/getUser", userController.getUser)
 router.put("/updateUser",userController.updateUser)
 router.delete("/deleteUser", userController.deleteUser)
